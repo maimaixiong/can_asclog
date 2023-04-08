312fff6c3199f147de173059ab001406a74372c4
maimaixiong <dinglx@uincore.net>
2023-04-08 23:15:48 +0800

    init version

---
+++ ASCReader.cc
@@ -0,0 +1,30 @@
+#include <filesystem>
+#include <fstream>
+#include <regex>
+#include <vector>
+#include <cstring>
+#include <ctime>
+
+#include <iostream>
+#include <sstream>
+
+// vim tips:
+//	!g++ %;./a.out
+
+class ASCReader {
+public:
+ 
+private:
+
+};
+
+int main() {
+
+  std::cerr << "ASCReader" <<std::endl;
+  std::cout << "ASCReader" <<std::endl;
+
+  ASCReader ascReader;
+  return 0;
+
+}
+
