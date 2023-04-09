8e7f0506a1ffcd1701850d0b816bb536dde143a8
maimaixiong <git@github.com>
2023-04-09 23:25:39 +0800

    date regex ok

--- ASCReader.cc
+++ ASCReader.cc
@@ -18,12 +18,45 @@ private:
 
 };
 
+int test_regex(void)
+{
+	std::ifstream infile("logfile.asc");
+	std::string line = "";
+
+	if (infile.is_open() ) {
+         int number;
+         for(; getline(infile, line); )
+         {
+		std::cout << line <<std::endl;
+            	if( line.find("date", 0) == 0 ) 
+			break;
+         }
+         infile.close();
+     }
+
+	std::regex date_regex("date(\\s+)(\\w+)\\s+(\\w+)(.*)[\r\n]");
+    	std::smatch match;
+
+	if ( std::regex_match(line, match, date_regex) ) {
+		std::cout << "--- matched ---" << std::endl << std::endl;
+	}
+
+	for (unsigned i=0; i<match.size(); ++i) {
+      		std::cout << "[" << match[i] << "] ";
+   	}
+	std::cout << std::endl;
+	
+	return 0;
+}
+
 int main() {
 
-  std::cerr << "ASCReader" <<std::endl;
+  //std::cerr << "ASCReader" <<std::endl;
   std::cout << "ASCReader" <<std::endl;
 
   ASCReader ascReader;
+
+  test_regex();
   return 0;
 
 }
