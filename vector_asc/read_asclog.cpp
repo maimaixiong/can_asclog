76f803a2ab36dc66ffaaa0269e8592bfa4b4a794
maimaixiong <dinglx@uincore.net>
2023-06-03 18:11:23 +0800

    vector asc and python-can

--- vector_asc/read_asclog.cpp
+++ vector_asc/read_asclog.cpp
@@ -5,6 +5,12 @@
 
 #include <Vector/ASC.h>
 
+void showCanMessage(Vector::ASC::CanMessage *CanMsg)
+{
+    std::cout << "--------------- CanMessage: --------------------"<<std::endl;
+    std::cout << CanMsg->id <<std::endl;
+}
+
 int main(int argc, char * argv[]){
     if (argc != 2) {
         std::cout << argv[0] << " <filename.asc>" <<std::endl;
@@ -18,18 +24,29 @@ int main(int argc, char * argv[]){
     Vector::ASC::File fileout;
     fileout.open("test.out", Vector::ASC::File::OpenMode::Write);
 
+    Vector::ASC::CanMessage * canMessage;
+
     /* parse log file */
     while (!filein.eof()) {
         Vector::ASC::Event * event;
         event = filein.read();
         assert((event != nullptr) || filein.eof());
         if (event != nullptr) {
+
+            std::cout << "eventType:" << event->eventType << std::endl;
+
             /* these values must be copied to File */
             switch (event->eventType) {
             case Vector::ASC::Event::EventType::FileDate: {
                 /*Vector::ASC::FileDate * fileDate = static_cast<Vector::ASC::FileDate *>(event);*/
                 fileout.language = filein.language;
                 fileout.date = filein.date;
+		std::cout << "--------------- FileDate --------------------"<<std::endl;
+                char time_buf[256];
+                (void) strftime(time_buf, sizeof (time_buf),
+                        "%m-%d-%Y %H:%M:%S (mon=%b)", &filein.date);
+                std::cout << filein.language << "   " << std::string(time_buf) << std::endl;
+                //std::cout << filein.language << "   ", std::put_time(filein.date, "%d-%m-%Y %H:%M:%S") << std::endl;
             }
             break;
             case Vector::ASC::Event::EventType::FileBaseTimestamps: {
@@ -40,10 +57,27 @@ int main(int argc, char * argv[]){
             }
             break;
             case Vector::ASC::Event::EventType::FileVersion: {
-                /*Vector::ASC::FileVersion * fileVersion = static_cast<Vector::ASC::FileVersion *>(event);*/
+                Vector::ASC::FileVersion * fileVersion = static_cast<Vector::ASC::FileVersion *>(event);
                 fileout.version = filein.version;
+		std::cout << "--------------- FileVersion  --------------------"<<std::endl;
+                //std::cout << (int16_t) fileVersion->versionMajor << (int16_t) fileVersion->versionMinor << (int16_t) fileVersion->versionPatch << std::endl;
+                std::cout
+                    << "// version"
+                    << ' ' << std::dec << (int16_t) fileVersion->versionMajor
+                    << '.' << std::dec << (int16_t) fileVersion->versionMinor
+                    << '.' << std::dec << (int16_t) fileVersion->versionPatch
+                <<std::endl;
+
+
             }
 	    break;
+            
+            case Vector::ASC::Event::EventType::CanMessage: {
+                canMessage = static_cast<Vector::ASC::CanMessage *>(event);
+                showCanMessage(canMessage);
+            }
+            break;
+
 	    default:
 	    break;
 	    }
---
+++ vector_asc/read_asclog.sh
@@ -0,0 +1,1 @@
+LD_LIBRARY_PATH=Vector_ASC-1.1.3-Linux/lib/ ./read_asclog $1
